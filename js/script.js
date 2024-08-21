575a8bb15e82e19f7dd439165331e66ff01442f4
zihnidurmush <134578064+zihnidurmush@users.noreply.github.com>
2024-08-21 13:38:29 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,11 +7,11 @@ const exchangeRateTxt = document.querySelector('.exchange-rate');
 
 for (let i = 0; i < dropList.length; i++) {
     for (let currency_code in country_code) {
-        let selected;
+        let selected = '';
         if (i === 0) {
-            selected = currency_code == 'TRY' ? 'selected' : '';
-        } else if (i == 1) {
-            selected = currency_code == 'BGN' ? 'selected' : '';
+            selected = currency_code === 'TRY' ? 'selected' : '';
+        } else if (i === 1) {
+            selected = currency_code === 'BGN' ? 'selected' : '';
         }
         
         let optionTagForCurrency = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
@@ -25,17 +25,23 @@ for (let i = 0; i < dropList.length; i++) {
 }
 
 function loadFlag(element) {
-    for(let code in country_code) {
+    for (let code in country_code) {
         if (code === element.value) {
             let imgTag = element.parentElement.querySelector('img');
             if (imgTag) {
-            imgTag.src = `https://flagsapi.com/${country_code[code]}/shiny/64.png`;
+                imgTag.src = `https://flagsapi.com/${country_code[code]}/shiny/64.png`;
             }
         }
     }
 }
 
 amountInput.addEventListener('input', () => {
+    let value = parseFloat(amountInput.value.trim());
+
+    if (isNaN(value) || value < 0) {
+        value = '0'; 
+    }
+    amountInput.value = value.toString();
     getExchangeRate();
 });
 
@@ -51,28 +57,26 @@ exchangeIcon.addEventListener('click', () => {
     loadFlag(fromCurrency);
     loadFlag(toCurrency);
     getExchangeRate();
-})
+});
 
 function getExchangeRate() {
-    let amountValue = amountInput.value;
-    
-    if (amountValue === '' || amountValue === '0') {
-        amountInput.value = '1';
-        amountValue = 1;
+    let amountValue = parseFloat(amountInput.value.trim());
+
+    if (isNaN(amountValue) || amountValue < 0) {
+        amountValue = 0; 
+        amountInput.value = amountValue.toString();
     }
-        
+
     let url = `https://v6.exchangerate-api.com/v6/edc87be85f1233921a235184/latest/${fromCurrency.value}`;
-    
+
     fetch(url)
     .then(response => response.json())
     .then(result => {
         let exchangeRate = result.conversion_rates[toCurrency.value];
-        
         let totalExchangeRate = (amountValue * exchangeRate).toFixed(2);
-        exchangeRateTxt.innerText = `${amountValue} ${fromCurrency.value} = ${totalExchangeRate} ${toCurrency.value}`;        
-    }).catch(() => {
-        exchangeRateTxt.innerText = 'Something went wrong';
+        exchangeRateTxt.innerText = `${amountValue} ${fromCurrency.value} = ${totalExchangeRate} ${toCurrency.value}`;
     })
+    .catch(() => {
+        exchangeRateTxt.innerText = 'Something went wrong';
+    });
 }
-
-
